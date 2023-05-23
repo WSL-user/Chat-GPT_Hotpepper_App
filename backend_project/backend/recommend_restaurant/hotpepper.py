f2490b2eae532eda07fedc9c16922a82617bb721
Sayaka Aoba <107929780+aobaaaaa@users.noreply.github.com>
2023-05-23 20:49:09 +0900

    Update hotpepper.py
    hotpepper.pyの変更

--- backend_project/backend/recommend_restaurant/hotpepper.py
+++ backend_project/backend/recommend_restaurant/hotpepper.py
@@ -1,25 +1,144 @@
 from .models import Restaurant
 import requests
+import json
+import pandas as pd
+from IPython import display
+import time
+from janome.tokenizer import Tokenizer
+from janome.analyzer import Analyzer
+from janome import tokenfilter, charfilter
+import re, regex
+
 HOTPEPPER_API_KEY = 'YOUR HOTPEPPER API KEY HERE'
+GET_restaurant_NUM = 100
+
+USE_SEARCH_FIELD_LIST = ['address','keyword', 'free_drink', 'free_food', 'private_room',
+                         'wifi', 'non_smoking', 'parking', 'night_view', 'karaoke', 'tv', 'lunch',
+                         'midnight', 'midnight_meal', 'pet', 'child',]
+
+USE_RETURN_FIELD_LIST = ['access', 'address', 'barrier_free','budget',
+                          'catch', 'child','close', 'coupon_urls', 
+                          'free_drink', 'free_food', 'genre', 'id',
+                          'lunch', 'midnight', 'mobile_access', 'name',
+                          'name_kana', 'non_smoking', 'open','parking', 
+                          'photo', 'small_area', 'wifi',]
+
+#
+CHECK_KETWORD_LIST = ['barrier_free', 'budget', 'child', 'free_drink', 
+                      'free_food', 'lunch', 'non_smoking', 'parking', 'wifi']
+
+CHECK_KEYWORD_HIRA_DIC = {('車椅子','バリアフリー'):'barrier_free',
+                          ('子供','子ども','こども'):'child',
+                          ('ワイファイ','wifi','wi-fi'):'wifi',
+                          ('のみ','飲み'):'free_drink',
+                          ('たべ','食べ'):'free_drink',
+                          ('ランチ'):'lunch',
+                          ('禁煙'):'non_smoking',
+                          ('車','駐車場','パーキング'):'parking',
+                          ('ペット','犬','猫'):'pet',
+                          ('個室'):'private_room',
+                          ('クーポン'):'',
+                          ('座敷'):'tatami',
+                          ('カクテル'):'cocktail',
+                          ('焼酎'):'shochu',
+                          ('日本酒'):'sake',
+                          ('ワイン'):'wine',
+                          ('夜景'):'night_view',
+                          ('tv','テレビ'):'tv',      
+                         }
+
+#文分割、単語抜き出し
+def  extra_use_words(sentence):
+    char_filters = [charfilter.UnicodeNormalizeCharFilter()]
+    token_filters = [tokenfilter.LowerCaseFilter(),
+                     tokenfilter.POSKeepFilter(['名詞', '形容詞', '副詞']),
+                     #tokenfilter.CompoundNounFilter(),
+                    ]
+    n_token_filters = [tokenfilter.LowerCaseFilter(),
+                       tokenfilter.POSKeepFilter(['名詞']),]
+    tokenizer = Tokenizer()
+
+    analyzer = Analyzer(char_filters=char_filters, token_filters=token_filters)
+    n_analyzer = Analyzer(char_filters=char_filters, token_filters=n_token_filters)
+
+    use_words_list  = [token.surface for token in analyzer.analyze(sentence)]
+    n_words_list  = [token.surface for token in n_analyzer.analyze(sentence)]
+
+    #print("N:",n_words_list)
+
+    return n_words_list, use_words_list
+
+#いらなそうな単語除去
+#おそらく最新の検索形式だと必要ない関数？
+def check_words(word):
+    not_use_word = ['店','駅', '近く', '雰囲気']
+    if word in not_use_word:
+        return False
+    else:
+        return True
+    
+
+def extra_keywords(sentence):
+    keywords_dic = {}
+    n_words_list, search_words_list = extra_use_words(sentence)
+    keyword = ''
+    
+    #bodyの一部を作成
+    for search_word in search_words_list:
+        use_flag = False
+        for keyword_list in CHECK_KEYWORD_HIRA_DIC.keys():
+            if search_word in keyword_list:
+                keywords_dic[CHECK_KEYWORD_HIRA_DIC[keyword_list]] = 1
+                use_flag = True
+                break
+        
+        #keyword部分作成(残った名詞全部をつなげる)
+        if (not use_flag) and (check_words(search_word) and (search_word in n_words_list)):
+            keyword += search_word + ' '
+
+    keywords_dic['keyword'] = keyword
+
+    return keywords_dic
+
+def make_body(search_words, region):
+    #改善の余地あり
+    body_part = extra_keywords(search_words)
+
+    #'keyword':search_words['keyword'],
+    body = {'key':HOTPEPPER_API_KEY,
+            'format':'json',
+            'address':region,
+            'count':GET_restaurant_NUM,
+            'type':"special",
+            'order':4, #初期値は4だが自明にする
+            }
+    body = body_part | body #辞書結合
+
+    #print("body:", body)
+
+    for filed in USE_SEARCH_FIELD_LIST:
+        if (filed in search_words) and (search_words[filed] != ''):
+            body[filed] = search_words[filed]
+
+    return body
+
+
 
 #ホットペッパーグルメに聞く関数
-def ask_hotpepper(keyword, adress,count=100):
+#search_sentence部分がおそらく適切な受け取り方になっていない？
+def ask_hotpepper(search_sentence, count=100):
     URL = 'http://webservice.recruit.co.jp/hotpepper/gourmet/v1/'
 
-    body = {
-      'key':HOTPEPPER_API_KEY,
-      ##
-      'adress': adress,
-      'keyword': keyword, #
-      'format':'json',
-      'count':count,
-      'type':"special"
-    }
+    ############仮　エラーにならないよう埋めている
+    #本当は引数として受け取りたい
+    region = ""
+    body = make_body(search_sentence, region)
     response = requests.get(URL,body)
 
     return response
 
 #ホットペッパーグルメの検索結果をデータベースに追加する関数
+####この関数はいじってないのでデータベースに保存するフィールドは増やす必要あり
 def insert_objects(hotpepper_response):
     shops = hotpepper_response.json()['results']['shop']
     if len(shops) == 0:
