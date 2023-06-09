60b7c2e40fb402d7819267168261925bfcd1f6d1
RyozoMasukawa <tyunibyokanja@gmail.com>
2023-06-09 14:00:19 +0900

    Concealed OPENAI API KEY

--- backend_project/backend/recommend_restaurant/chatGPT.py
+++ backend_project/backend/recommend_restaurant/chatGPT.py
@@ -2,7 +2,7 @@ import openai
 from django.forms.models import model_to_dict
 
 # Create your views here.
-openai.api_key='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'
+openai.api_key='YOUR OPENAI API KEY HERE'
 
 #chatGPTに聞く関数
 def ask_chatGPT(restaurant):
